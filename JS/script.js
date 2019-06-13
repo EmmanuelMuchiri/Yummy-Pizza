2b0c387f00ee0d4fd58cf4587147d9afb1ecbb5b
Emmanuel Muchiri <Emmanuel.Muchiri@outlook.com>
2019-06-13 22:20:26 +0300

    added the business logic part one of the application to determine the price of the pizza based on the size,cheese and toppings and tested on the terminal to ensure that it is working

--- JS/script.js
+++ JS/script.js
@@ -1,34 +1,40 @@
-var totalPrice = [];
-
-function placeOrder(size, cheese) {
-  this.size = defaultSize;
+//var totalPrice = [];
+//------Constructor Placeorder created with it's properties----//
+function Placeorder(size, cheese, topping) {
+  this.size = size;
   this.cheese = cheese;
-  this.sauce = sauce;
-  this.vegeOne = 1;
-  this.vegeTwo = 1;
-  this.meat = 2;
-  this.price = 0;
-  this.priceTwo = 3;
+  this.topping = topping;
+  this.price = 0
 }
-
-Order.prototype.costOfPizza = function() {
-  if (this.size === "Small") {
-    this.price += 6;
-  } else if (this.size === "Medium") {
-    this.price += 9;
-  } else if (this.size === "Large") {
-    this.price += 12;
+//--Arrays to declare the pizzaSize,pizzaCheese and pizzaTopping to be used in the prototype CostOfPizza--//
+var pizzaSize = ["Small", "Medium", "Large"];
+var pizzaCheese = ["Standard", "More Cheese", "Most Cheese"];
+var pizzaTopping = ["Bacon", "Chicken", "beef", "vegetables"];
+//-Prototype CostOfPizza created to determine the price based on the three properties i.e size,cheese,topping-//
+Placeorder.prototype.CostOfPizza = function() {
+  if (this.size === pizzaSize[0]) {
+    this.price = price + 300;
+  } else if (this.size === pizzaSize[1]) {
+    this.price = price + 400;
+  } else if (this.size === pizzaSize[2]) {
+    this.price = price + 500;
   }
-  if (this.cheese === "cheese") {
-    this.price += 1;
-  } else if (this.cheese === "standard cheese") {
-    this.price += 0.5;
-  } else if (this.cheese === "more cheese") {
-    this.price += 1.5;
+  if (this.cheese === pizzaCheese[0]) {
+    this.price += 100;
+  } else if (this.cheese === pizzaCheese[1]) {
+    this.price += 200;
+  } else if (this.cheese === pizzaCheese[2]) {
+    this.price += 300;
   }
-  this.price += this.sauce;
-  this.price += this.vegeOne;
-  this.price += this.vegeTwo;
-  this.price += this.meat;
+  if (this.topping === pizzaTopping[0]) {
+    this.price += 100;
+  } else if (this.topping === pizzaTopping[1]) {
+    this.price += 200;
+  } else if (this.topping === pizzaTopping[2]) {
+    this.price += 300;
+  } else if (this.topping === pizzaTopping[3]) {
+    this.price += 50;
+}
   return this.price;
 }
+//----End Of the Prototype CostOfPizza---//
