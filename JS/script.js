dd2576026c7327b86ae9f423fc3f8efe6472ab54
Emmanuel Muchiri <Emmanuel.Muchiri@outlook.com>
2019-06-13 20:47:18 +0300

    created constructor order with it's properties and created a prototype to calculate prize of a pizza after inheriting the properties of the order

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,34 @@
-$(document).ready() {
-  ("body").click() {
-    alert("my js is working");
+var totalPrice = [];
+
+function placeOrder(size, cheese) {
+  this.size = defaultSize;
+  this.cheese = cheese;
+  this.sauce = sauce;
+  this.vegeOne = 1;
+  this.vegeTwo = 1;
+  this.meat = 2;
+  this.price = 0;
+  this.priceTwo = 3;
+}
+
+Order.prototype.costOfPizza = function() {
+  if (this.size === "Small") {
+    this.price += 6;
+  } else if (this.size === "Medium") {
+    this.price += 9;
+  } else if (this.size === "Large") {
+    this.price += 12;
   }
+  if (this.cheese === "cheese") {
+    this.price += 1;
+  } else if (this.cheese === "standard cheese") {
+    this.price += 0.5;
+  } else if (this.cheese === "more cheese") {
+    this.price += 1.5;
+  }
+  this.price += this.sauce;
+  this.price += this.vegeOne;
+  this.price += this.vegeTwo;
+  this.price += this.meat;
+  return this.price;
 }
